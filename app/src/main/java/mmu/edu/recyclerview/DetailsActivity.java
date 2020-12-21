f06bc2a3750e939eba91f60e31169c0a859492c6
nuramalina99 <nuramalinarosle184@gmail.com>
2020-12-22 03:29:19 +0800

    This is tuto 3 actionbar.

--- app/src/main/java/mmu/edu/recyclerview/DetailsActivity.java
+++ app/src/main/java/mmu/edu/recyclerview/DetailsActivity.java
@@ -1,12 +1,19 @@
 package mmu.edu.recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
+    private String url;
+    private String name;
 
 
     @Override
@@ -14,9 +21,30 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         String name = getIntent().getStringExtra("name");
-        String url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
+
+        url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
+        Toast.makeText(this,url, Toast.LENGTH_SHORT).show();
         WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi there, I'm sending the details for "+name+" at "+ url);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
